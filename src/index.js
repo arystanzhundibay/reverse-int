e59868ad27ffee3e066174b3d9b9b2b70807924b
Arystan Zhundibay <96076880+arystanzhundibay@users.noreply.github.com>
2022-01-13 17:53:00 +0500

    docs: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let result = '';
+    let strN = String(Math.abs(n));
+    for (var i = 0; i < strN.length; i += 1) {
+        result = strN[i] + result
+    }
+    return result;
 }
